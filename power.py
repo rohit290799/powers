0caad7f56cec3e3428acf899f0b46cb72294ef0c
rohit290799 <86189618+rohit290799@users.noreply.github.com>
2022-11-14 23:54:05 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def MyFun(x):
+    return 2 ** x
+print("Enter Number of Term: ")
+take = int(input())
+print()
+for i in range(take):
+    print( MyFun(i))
+
+
+
+
